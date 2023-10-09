918bc06e0e0061b5cfb0b9b6e1747e207ca71785
7086cmd <1371077054@qq.com>
2023-10-09 17:15:08 +0800

    feat: api add get activities

---
+++ src/api/activity/read.ts
@@ -0,0 +1,55 @@
+import axios from '@/plugins/axios'
+import type { Response } from '@/../@types/response'
+import type { ActivityInstance } from '@/../@types/activity'
+import { ElNotification } from 'element-plus'
+
+export async function getAllActivities(
+  range: 'in-class' | 'campus',
+  filter: {
+    type: 'special' | 'specified' | 'off-campus' | 'all'
+  }
+) {
+  const result = (
+    await axios('/activities', {
+      params: {
+        type: filter.type,
+        range
+      }
+    })
+  ).data as Response<ActivityInstance[]>
+  if (result.status === 'error') {
+    ElNotification({
+      title: `获取义工列表失败（${result.code}）`,
+      message: result.message,
+      type: 'error'
+    })
+    return
+  }
+  return result.data
+}
+
+export async function getUserActivities(id: number) {
+  const result = (await axios(`/user/${id}/activities`)).data as Response<ActivityInstance[]>
+  if (result.status === 'error') {
+    ElNotification({
+      title: `获取用户义工列表失败（${result.code}）`,
+      message: result.message,
+      type: 'error'
+    })
+    return
+  }
+  return result.data
+}
+
+export async function getActivity(id: string) { // id: ObjectId
+  const result = (await axios(`/activity/${id}`)).data as Response<ActivityInstance>
+  if (result.status === 'error') {
+    ElNotification({
+      title: `获取义工失败（${result.code}）`,
+      message: result.message,
+      type: 'error'
+    })
+    return
+  }
+  return result.data
+}
