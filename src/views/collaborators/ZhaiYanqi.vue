2222ed5e1ef2293fc7291df2316f36ebfbb6d432
diyanqi <diyanqi07@gmail.com>
2023-10-29 22:09:23 +0800

    fix: 无法滚动问题&typo ~~&私货~~

--- src/views/collaborators/ZhaiYanqi.vue
+++ src/views/collaborators/ZhaiYanqi.vue
@@ -1,3 +1,9 @@
 <template>
-  <p class="text-2xl">2023 级 翟彦祺</p>
+  <p class="text-2xl">2023 级 翟彦棋</p>
+  <br/>
+  <a href="https://github.com/diyanqi" target="_blank"><img src="https://img.shields.io/badge/-@diyanqi-181717?style=for-the-badge&logo=github&logoColor=white&link=https://github.com/diyanqi"/></a>
+  <br/>
+  <a href="https://www.amzcd.top" target="_blank"><img src="https://img.shields.io/badge/-www.amzcd.top-orange?style=for-the-badge&logo=rss&logoColor=white&link=https://www.amzcd.top"/></a>
+  <br/>
+  <img src="https://img.shields.io/badge/ikun%20test-passed-blue?style=for-the-badge"/>
 </template>
