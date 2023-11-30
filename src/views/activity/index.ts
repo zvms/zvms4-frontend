960d14d4bd97922eb73ba5d3e42f46d5356e8c8e
Wu Chengyu <54303040+7086cmd@users.noreply.github.com>
2023-11-30 21:35:13 +0800

    refact(/src/views/): use index

---
+++ src/views/activity/index.ts
@@ -0,0 +1,3 @@
+export { default as CreateActivity } from './CreateActivity.vue'
+export { default as CreatePage } from './CreatePage.vue'
+export { default as UserRegister } from './UserRegister.vue'
