3e3c8f61dbc618ad2d7439b8f56537abc7914f77
Wu Chengyu <54303040+7086cmd@users.noreply.github.com>
2023-12-03 10:42:59 +0800

    refact: seal inside apis for notifications

--- src/api/export.ts
+++ src/api/export.ts
@@ -1,2 +1,3 @@
 export * as activity from './activity'
 export * as user from './user'
+export * as notification from './notification'
--- src/components/notifications/ZNotificationList.vue
+++ src/components/notifications/ZNotificationList.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import { ElCard, ElTable, ElTableColumn } from 'element-plus';
-import { useWindowSize } from '@vueuse/core';
-import {ref, watch} from 'vue'
-import {getMyNotifications} from '@/api/notifications/read'
-import type { Broadcast } from '@/../@types/broadcast';
-import type { Ref } from 'vue';
+import { ElCard, ElTable, ElTableColumn } from 'element-plus'
+import { useWindowSize } from '@vueuse/core'
+import { ref, watch } from 'vue'
+import type { BroadcastInstance } from '@/../@types/broadcast'
+import api from '@/api'
+import { useUserStore } from '@/stores/user'
 
 const loading = ref(true)
+
+const user = useUserStore()
 
 // Deal with window size
 const { width, height } = useWindowSize()
@@ -15,24 +17,26 @@ watch(width, () => {
   tableMaxHeight.value = height.value * 0.56
 })
 
-const notifications: Ref<Broadcast[]> = ref([])
+const notifications = ref<BroadcastInstance[]>([])
 
-getMyNotifications('0').then((res: Broadcast[])=>{
-    loading.value = false
-    notifications.value = res ?? []
+console.log(api.notification)
+
+api.notification.read.mine(user._id).then((res: BroadcastInstance[]) => {
+  loading.value = false
+  notifications.value = res ?? []
 })
 </script>
 
 <template>
-    <div :class="['card', 'pr-8', width < height ? 'pl-6' : '']" v-loading="loading">
-        <ElCard shadow="never" :data="notifications">
-            <ElTable>
-                <ElTableColumn type="expand" label="Title">
-                    <template #default="{row}">
-                        <strong>{{ row.title }}</strong>
-                    </template>
-                </ElTableColumn>
-            </ElTable>
-        </ElCard>
-    </div>
-</template> 
+  <div :class="['card', 'pr-8', width < height ? 'pl-6' : '']" v-loading="loading">
+    <ElCard shadow="never" :data="notifications">
+      <ElTable>
+        <ElTableColumn type="expand" label="Title">
+          <template #default="{ row }">
+            <strong>{{ row.title }}</strong>
+          </template>
+        </ElTableColumn>
+      </ElTable>
+    </ElCard>
+  </div>
+</template>
