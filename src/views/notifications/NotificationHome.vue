fe656099196cf44c2a6904c440c6851c7cac37a5
Wu Chengyu <54303040+7086cmd@users.noreply.github.com>
2024-01-13 20:06:44 +0800

    fix: rename all `broadcast` and remove useless imports from `NotificationHome`

--- src/views/notifications/NotificationHome.vue
+++ src/views/notifications/NotificationHome.vue
@@ -1,13 +1,8 @@
 <script lang="ts" setup>
 import ZNotificationList from '@/components/notifications/ZNotificationList.vue'
-import { ElButton } from 'element-plus'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
-
-const jumpToCreate = () => {
-  router.push('/notifications/create')
-}
 </script>
 
 <template>
