a17b92d6caa8a2f27a25783687fc21f7bbe50308
Wu Chengyu <54303040+7086cmd@users.noreply.github.com>
2024-01-15 16:49:24 +0800

    feat: permission confirm of creating activities

--- src/components/activity/ZActivityPage.vue
+++ src/components/activity/ZActivityPage.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import type { ActivityInstance } from '@/../@types/activity'
-import { ZActivityCard, ZActivityHistory, ZActivityMemberList } from '.'
+import { ZActivityCard, ZActivityHistory, ZActivityMemberList, ZActivityMember } from '.'
 import { ref, withDefaults } from 'vue'
 import { useWindowSize } from '@vueuse/core'
-import { ElPageHeader } from 'element-plus'
+import { ElPageHeader, ElButton, ElSpace } from 'element-plus'
 import { ArrowLeft } from '@element-plus/icons-vue'
 import { useUserStore } from '@/stores/user'
+import { StreamlineInterfaceUserEditActionsCloseEditGeometricHumanPencilPersonSingleUpUserWrite } from '@/icons'
+import dayjs from 'dayjs'
+import { Clock, Calendar } from '@element-plus/icons-vue'
 
 const user = useUserStore()
 
@@ -17,13 +20,6 @@ const props = withDefaults(
 )
 
 const { activity } = props
-
-// const availibility = ref(
-//   user.position.includes('department') ||
-//     user.position.includes('auditor') ||
-//     user.position.includes('admin') ||
-//     user.position.includes('secretary')
-// )
 </script>
 
 <template>
@@ -31,6 +27,26 @@ const { activity } = props
     <ElPageHeader v-if="activity._id" :icon="ArrowLeft" @back="() => $router.back()" class="py-4">
       <template #content>
         {{ activity.name }}
+      </template>
+      <template #extra>
+        <ElSpace>
+          <ZActivityMember
+            :id="activity.creator"
+            :icon="
+              StreamlineInterfaceUserEditActionsCloseEditGeometricHumanPencilPersonSingleUpUserWrite
+            "
+          />
+          <ElButton
+            text
+            bg
+            round
+            size="small"
+            type="info"
+            :icon="activity.type === 'specified' ? Calendar : Clock"
+          >
+            {{ dayjs(activity.createdAt).format('YYYY-MM-DD') }}
+          </ElButton>
+        </ElSpace>
       </template>
     </ElPageHeader>
     <p class="text-gray-500 dark:text-gray-400 px-4">
