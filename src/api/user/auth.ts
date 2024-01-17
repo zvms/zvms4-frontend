69d4e9e6590faffab90bab7d7dd05cce9c272b7b
7086cmd <1371077054@qq.com>
2024-01-17 16:35:37 +0800

    feat: remove the md5 encryption and add the rsa

--- src/api/user/auth.ts
+++ src/api/user/auth.ts
@@ -1,11 +1,17 @@
 import axios from '@/plugins/axios'
-import md5 from 'md5'
 import type { Response } from '@/../@types/response'
 import type { LoginResult } from '@/../@types/login'
 import { ElNotification } from 'element-plus'
+import JSEncrypt from 'jsencrypt'
 
-export async function getRSAPublicCert() {
-  const result = (await axios('/cert')) as Response<string>
+export async function getRSAPublicCert(): Promise<JSEncrypt | undefined> {
+  const result = (await axios('/cert', {
+    method: 'GET',
+    params: {
+      type: 'public',
+      method: 'RSA'
+    }
+  })) as Response<string>
   if (result.status === 'error') {
     ElNotification({
       title: '获取公钥错误（' + result.code + '）',
@@ -14,15 +20,21 @@ export async function getRSAPublicCert() {
     })
     return
   }
-  return result.data
+  const encrypt = new JSEncrypt()
+  encrypt.setPublicKey(result.data)
+  return encrypt
 }
 
-async function UserLogin(user: string, password: string) {
+async function UserLogin(user: string, password: string, term: 'long' | 'short' = 'long') {
+  const encrypt = await getRSAPublicCert()
+  if (!encrypt) return
+  const password_encrypted = encrypt?.encrypt(password)
   const result = (await axios('/user/auth', {
     method: 'POST',
     data: {
       userident: user.toString(),
-      password: md5(password)
+      password: password_encrypted,
+      role: term
     }
   })) as Response<LoginResult>
   if (result.status === 'error') {
@@ -37,4 +49,3 @@ async function UserLogin(user: string, password: string) {
 }
 
 export { UserLogin as useLongTermAuth }
-export { useShortTermToken as useShortTermAuth } from './short-term'
