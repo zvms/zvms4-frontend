59fa7f67d6d6da83ec19897ee02c9f26d6abb055
Ethan Goh <54303040+7086cmd@users.noreply.github.com>
2024-04-02 16:59:50 +0800

    fix: some class get 'error'

--- src/components/activity/ZActivityList.vue
+++ src/components/activity/ZActivityList.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { ActivityInstance, ActivityMember } from '@zvms/zvms4-types'
+import type { ActivityInstance, ActivityMember } from "@zvms/zvms4-types";
 import {
   ElDialog,
   ElTable,
@@ -9,122 +9,136 @@ import {
   ElCard,
   ElInput,
   ElSkeleton,
-  ElDivider
-} from 'element-plus'
-import { onMounted, ref, toRefs, watch } from 'vue'
-import { useUserStore } from '@/stores/user'
-import dayjs from 'dayjs'
-import { Box, Search, PieChart, Refresh, Plus, EditPen, View } from '@element-plus/icons-vue'
-import { useWindowSize } from '@vueuse/core'
-import { useI18n } from 'vue-i18n'
-import { getActivity } from './getActivity'
+  ElDivider,
+} from "element-plus";
+import { onMounted, ref, toRefs, watch } from "vue";
+import { useUserStore } from "@/stores/user";
+import dayjs from "dayjs";
+import {
+  Box,
+  Search,
+  PieChart,
+  Refresh,
+  Plus,
+  EditPen,
+  View,
+} from "@element-plus/icons-vue";
+import { useWindowSize } from "@vueuse/core";
+import { useI18n } from "vue-i18n";
+import { getActivity } from "./getActivity";
 import {
   ZActivityImpressionDrawer,
   ZActivityType,
   ZActivityStatus,
   ZActivityDuration,
-  ZActivityCard
-} from '@/components'
-import { useRouter } from 'vue-router'
-import { Write } from '@icon-park/vue-next'
+  ZActivityCard,
+} from "@/components";
+import { useRouter } from "vue-router";
+import { Write } from "@icon-park/vue-next";
 
-const { t } = useI18n()
-const { width, height } = useWindowSize()
-const router = useRouter()
+const { t } = useI18n();
+const { width, height } = useWindowSize();
+const router = useRouter();
 
-const user = useUserStore()
+const user = useUserStore();
 
 const props = defineProps<{
-  role: 'campus' | 'class' | 'mine'
-}>()
+  role: "campus" | "class" | "mine";
+}>();
 
-const activePage = ref(1)
-const pageSize = ref(8)
-const size = ref(0)
+const activePage = ref(1);
+const pageSize = ref(8);
+const size = ref(0);
 
-const { role } = toRefs(props)
-const loading = ref(true)
-const inital = ref(true)
-const searchWord = ref('')
-const query = ref('')
+const { role } = toRefs(props);
+const loading = ref(true);
+const inital = ref(true);
+const searchWord = ref("");
+const query = ref("");
 
-const activities = ref<ActivityInstance[]>([])
+const activities = ref<ActivityInstance[]>([]);
 
 function refresh() {
-  loading.value = true
-  getActivity(user._id, role.value, activePage.value, pageSize.value, query.value)
+  loading.value = true;
+  getActivity(
+    user._id,
+    role.value,
+    activePage.value,
+    pageSize.value,
+    query.value
+  )
     .then((res) => {
       if (res && res?.data.length !== 0) {
-        activities.value = res?.data
-        size.value = res?.size
-        loading.value = false
+        activities.value = res?.data;
+        size.value = res?.size;
+        loading.value = false;
       } else {
-        activities.value = []
-        loading.value = false
-        size.value = 0
+        activities.value = [];
+        loading.value = false;
+        size.value = 0;
       }
-      inital.value = false
+      inital.value = false;
     })
     .catch(() => {
-      loading.value = false
-      inital.value = false
-    })
+      loading.value = false;
+      inital.value = false;
+    });
 }
 
-onMounted(refresh)
+onMounted(refresh);
 
-watch(activePage, refresh)
-watch(pageSize, refresh)
+watch(activePage, refresh);
+watch(pageSize, refresh);
 watch(query, () => {
-  activePage.value = 1
-  refresh()
-})
+  activePage.value = 1;
+  refresh();
+});
 
-const registerForSpecified = ref(false)
+const registerForSpecified = ref(false);
 
-const statusFilter = ['draft', 'pending', 'rejected', 'effective', 'refused']
+const statusFilter = ["draft", "pending", "rejected", "effective", "refused"];
 
-const tableMaxHeight = ref(height.value * 0.56)
+const tableMaxHeight = ref(height.value * 0.56);
 
 watch(width, () => {
-  tableMaxHeight.value = height.value * 0.56
-})
+  tableMaxHeight.value = height.value * 0.56;
+});
 
-const items = ref<ActivityInstance[]>([])
+const items = ref<ActivityInstance[]>([]);
 
 onSortChange({
-  column: 'date',
-  order: 'ascending'
-})
+  column: "date",
+  order: "ascending",
+});
 
 function onSortChange({ column, order }: { column: string; order: string }) {
-  activePage.value = 1
-  if (column === 'date') {
+  activePage.value = 1;
+  if (column === "date") {
     items.value.sort((a, b) => {
-      if (order === 'ascending') {
-        return dayjs(a.date).isBefore(dayjs(b.date)) ? -1 : 1
+      if (order === "ascending") {
+        return dayjs(a.date).isBefore(dayjs(b.date)) ? -1 : 1;
       } else {
-        return dayjs(a.date).isBefore(dayjs(b.date)) ? 1 : -1
+        return dayjs(a.date).isBefore(dayjs(b.date)) ? 1 : -1;
       }
-    })
-  } else if (column === 'name') {
+    });
+  } else if (column === "name") {
     items.value.sort((a, b) => {
-      if (order === 'ascending') {
-        return a.name.localeCompare(b.name)
+      if (order === "ascending") {
+        return a.name.localeCompare(b.name);
       } else {
-        return b.name.localeCompare(a.name)
+        return b.name.localeCompare(a.name);
       }
-    })
+    });
   }
 }
 
 watch(
   activities,
   () => {
-    items.value = activities.value
+    items.value = activities.value;
   },
   { immediate: true }
-)
+);
 </script>
 
 <template>
@@ -154,11 +168,19 @@ watch(
           round
           @click="router.push('/activity/create')"
         >
-          {{ t('nav.create') }}
+          {{ t("nav.create") }}
         </ElButton>
         <ElDivider direction="vertical" />
-        <ElButton type="primary" round class="px-1" text bg :icon="Refresh" @click="refresh">
-          {{ t('activity.form.actions.refresh') }}
+        <ElButton
+          type="primary"
+          round
+          class="px-1"
+          text
+          bg
+          :icon="Refresh"
+          @click="refresh"
+        >
+          {{ t("activity.form.actions.refresh") }}
         </ElButton>
         <ElDivider direction="vertical" />
         <ElButton
@@ -170,9 +192,17 @@ watch(
           round
           :disabled="!user.position.includes('admin')"
         >
-          {{ t('activity.export.name') }}
+          {{ t("activity.export.name") }}
         </ElButton>
-        <ElButton v-else :icon="PieChart" type="warning" text bg circle disabled />
+        <ElButton
+          v-else
+          :icon="PieChart"
+          type="warning"
+          text
+          bg
+          circle
+          disabled
+        />
       </div>
       <ElTable
         :max-height="tableMaxHeight"
@@ -183,13 +213,18 @@ watch(
       >
         <ElTableColumn type="expand">
           <template #default="{ row }">
-            <ZActivityCard :_id="row._id" :mode="role" :perspective="user._id" @refresh="refresh" />
+            <ZActivityCard
+              :_id="row._id"
+              :mode="role"
+              :perspective="user._id"
+              @refresh="refresh"
+            />
           </template>
         </ElTableColumn>
         <ElTableColumn prop="name" :label="t('activity.form.name')" />
         <ElTableColumn prop="date" :label="t('activity.form.date')" sortable>
           <template #default="{ row }">
-            {{ dayjs(row.date).format('YYYY-MM-DD') }}
+            {{ dayjs(row.date).format("YYYY-MM-DD") }}
           </template>
         </ElTableColumn>
         <ElTableColumn
@@ -200,7 +235,7 @@ watch(
             { text: t('activity.type.specified.name'), value: 'specified' },
             { text: t('activity.type.social.name'), value: 'social' },
             { text: t('activity.type.scale.name'), value: 'scale' },
-            { text: t('activity.type.special.name'), value: 'special' }
+            { text: t('activity.type.special.name'), value: 'special' },
           ]"
           :filter-method="(value, row) => row.type === value"
         >
@@ -244,15 +279,18 @@ watch(
             role === 'mine' ||
             role === 'class' ||
             (role === 'campus' &&
-              (user.position.includes('admin') || user.position.includes('department')))
+              (user.position.includes('admin') ||
+                user.position.includes('department')))
           "
           :label="t('activity.status.title')"
           :filters="
             statusFilter
-              .filter((x) => role === 'mine' || (x !== 'rejected' && x !== 'draft'))
+              .filter(
+                (x) => role === 'mine' || (x !== 'rejected' && x !== 'draft')
+              )
               .map((x) => ({
                 text: t('activity.status.' + x),
-                value: x
+                value: x,
               }))
           "
           :filter-method="
@@ -273,7 +311,8 @@ watch(
               v-else
               :type="row.status"
               :modifiable="
-                (role === 'class' && (row.type === 'social' || row.type === 'scale')) ||
+                (role === 'class' &&
+                  (row.type === 'social' || row.type === 'scale')) ||
                 user.position.includes('admin') ||
                 user.position.includes('department')
               "
@@ -297,31 +336,36 @@ watch(
             <ElButton
               :icon="Write"
               v-if="
+                role === 'mine' && 
                 ['draft', 'rejected'].includes(
-                  (row as ActivityInstance).members.find((x: ActivityMember) => x._id === user._id)
+                  (row as ActivityInstance).members?.find((x: ActivityMember) => x._id === user._id)
                     ?.status ?? ''
-                ) && role === 'mine'
+                )
               "
               text
               bg
               type="primary"
-              @click="router.push(`/activity/details/${row._id}/impression/${role}`)"
+              @click="
+                router.push(`/activity/details/${row._id}/impression/${role}`)
+              "
             >
-              {{ t('activity.impression.actions.write') }}
+              {{ t("activity.impression.actions.write") }}
             </ElButton>
             <ElButton
               :icon="EditPen"
               v-else-if="
                 role === 'campus' &&
                 (user.position.includes('admin') || user.position.includes('auditor')) &&
-                row.members.filter((x: ActivityMember) => x.status === 'pending').length > 0
+                row.members.filter((x: ActivityMember) => x.status === 'pending').length > 0 || role === 'class'
               "
               type="danger"
               text
               bg
-              @click="router.push(`/activity/details/${row._id}/impression/${role}`)"
+              @click="
+                router.push(`/activity/details/${row._id}/impression/${role}`)
+              "
             >
-              {{ t('activity.impression.actions.reflect') }}
+              {{ t("activity.impression.actions.reflect") }}
             </ElButton>
             <ElButton
               :icon="View"
@@ -329,9 +373,11 @@ watch(
               text
               bg
               type="info"
-              @click="router.push(`/activity/details/${row._id}/impression/${role}`)"
+              @click="
+                router.push(`/activity/details/${row._id}/impression/${role}`)
+              "
             >
-              {{ t('activity.impression.actions.view') }}
+              {{ t("activity.impression.actions.view") }}
             </ElButton>
           </template>
         </ElTableColumn>
