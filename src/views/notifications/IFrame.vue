4c980dd8c1e3fb96e5f1e0ee263aa9d71fa5ae26
7086cmd <54303040+7086cmd@users.noreply.github.com>
2024-04-28 16:15:11 +0800

    feat: modify available ports

--- src/views/notifications/IFrame.vue
+++ src/views/notifications/IFrame.vue
@@ -1,32 +1,36 @@
 <script setup lang="ts">
 import { useUserStore } from '@/stores/user'
-import { ref } from 'vue';
+import { ref } from 'vue'
 import { useRoute } from 'vue-router'
 import dayjs from 'dayjs'
-import { ElResult, ElEmpty } from 'element-plus';
+import { ElResult, ElEmpty } from 'element-plus'
 
 const route = useRoute()
 const user = useUserStore()
 
-const availablePorts = [5244, 1001, 1002]
+const availablePorts = [5246, 5000, 5173]
 const availableTime = [
   // From 6:30 to 7:00
   { start: '6:30', end: '7:00' },
   // From 12:00 to 12:40
   { start: '12:00', end: '12:40' },
   // From 17:15 to 17:45
-  { start: '17:15', end: '17:45' },
+  { start: '16:15', end: '17:45' },
   // From 21:30 to 22:00
   { start: '21:30', end: '22:00' }
 ]
 
-const port = parseInt((route.params.port).toString())
+const port = parseInt(route.params.port.toString())
 const available = availablePorts.includes(port)
 
 const isAvailableTime = availableTime.some(({ start, end }) => {
   const now = dayjs()
-  const startTime = dayjs(now).set('hour', Number(start.split(':')[0])).set('minute', Number(start.split(':')[1]))
-  const endTime = dayjs(now).set('hour', Number(end.split(':')[0])).set('minute', Number(end.split(':')[1]))
+  const startTime = dayjs(now)
+    .set('hour', Number(start.split(':')[0]))
+    .set('minute', Number(start.split(':')[1]))
+  const endTime = dayjs(now)
+    .set('hour', Number(end.split(':')[0]))
+    .set('minute', Number(end.split(':')[1]))
   return now.isAfter(startTime) && now.isBefore(endTime)
 })
 
@@ -40,13 +44,16 @@ if (available && isAvailableTime) {
 
 const limit = 8 // Refresh every 8 minutes
 
-setInterval(() => {
-  const now = dayjs()
-  const next = dayjs(now).add(limit, 'minute')
-  if (now.isAfter(next)) {
-    location.reload()
-  }
-}, limit * 60 * 1000)
+setInterval(
+  () => {
+    const now = dayjs()
+    const next = dayjs(now).add(limit, 'minute')
+    if (now.isAfter(next)) {
+      location.reload()
+    }
+  },
+  limit * 60 * 1000
+)
 </script>
 
 <template>
