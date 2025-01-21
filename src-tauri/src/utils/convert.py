0e543338f931d784be5d2ee96fbcd1f6404c37cc
Ethan Goh <7086cmd@gmail.com>
2025-01-21 16:34:50 +0800

    chore: cleanup useless files

--- src-tauri/src/utils/convert.py
+++
@@ -1,9 +0,0 @@
-def arrow_to_excel(arrow_batch, dest: str):
-    import polars as pl
-    batch = pl.from_arrow(arrow_batch)
-    batch.write_excel(dest)
-
-def excel_to_arrow(src: str):
-    import polars as pl
-    df = pl.read_excel(src)
-    return df.to_arrow()
