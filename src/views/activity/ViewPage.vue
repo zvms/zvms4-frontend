0b7b638c77256848ab3bfc804593115e24c1acb2
Zcraft_ <153985444+Zcraft3793@users.noreply.github.com>
2025-03-04 22:14:58 +0800

    File no longer used

--- src/views/activity/ViewPage.vue
+++ src/views/activity/ViewPage.vue
@@ -1,17 +1,1 @@
-<script setup lang="ts">
-import { ZActivityList } from '@/components'
-import { useRoute } from 'vue-router'
-import { ref } from 'vue'
 
-const route = useRoute()
-
-const page = ref<'' | 'class' | 'campus'>(
-  route.path.replace('/activities/', '').split('/')[0] as '' | 'class' | 'campus'
-)
-
-const type = ref<'mine' | 'class' | 'campus'>(page.value ? page.value : 'mine')
-</script>
-
-<template>
-  <ZActivityList class="mx-12" :role="type" />
-</template>
