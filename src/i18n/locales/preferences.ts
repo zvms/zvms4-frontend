ea53c48494f41749803b5dcb5b5d2e91a5cd8b53
Zcraft_ <153985444+Zcraft3793@users.noreply.github.com>
2025-03-11 11:48:04 +0800

    file no longer used

--- src/i18n/locales/preferences.ts
+++ src/i18n/locales/preferences.ts
@@ -1,15 +1,5 @@
 export const zhCN = {
-  fonts: {
-    serif: '宋体',
-    sans: '黑体',
-    mono: '等宽字体'
-  }
 }
 
 export const enUS = {
-  fonts: {
-    serif: 'Serif',
-    sans: 'Sans',
-    mono: 'Monospace'
-  }
 }
