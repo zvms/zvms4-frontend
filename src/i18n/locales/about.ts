78b3365859904e85c8898442c4df7d36b49af9f8
Zcraft_ <153985444+Zcraft3793@users.noreply.github.com>
2025-03-11 11:17:31 +0800

    remove extra spaces

--- src/i18n/locales/about.ts
+++ src/i18n/locales/about.ts
@@ -9,10 +9,8 @@ export const enUS = {
     license: 'License',
     version: {
       '0': 'Version',
-      '1': 'is developed by ',
-      '2': '. Thanks!',
-      stack: 'Tech Stack: ',
-      repo: 'Repository: '
+      '1': 'is developed by',
+      '2': '. Thanks!'
     },
     thank: {
       mean: 'Meanwhile, thanks to',
@@ -25,14 +23,10 @@ export const enUS = {
       '1': 'also participated (or participating) maintenance.'
     }
   },
-  switch: {
-    real: 'Real Name',
-    nick: 'Nickname'
-  },
   privacy: {
     title: 'Privacy Policy',
     content:
-      'This site partners with Microsoft Clarity and Microsoft Advertising to capture how you use and interact with this site through behavioral metrics, heatmaps, and session replays to improve your browsing experience on this site. We use first and third-party cookies and other tracking technologies to obtain website usage data to determine the popularity of this site and online activities. Additionally, we use this information for political and educational management and review, website optimization, fraud/security purposes, and advertising. For more information on how Microsoft collects and uses your data, please visit the Microsoft Privacy Statement (https://privacy.microsoft.com/privacystatement).'
+      'This site partners with Microsoft Clarity and Microsoft Advertising to capture how you use and interact with this site through behavioral metrics, heatmaps, and session replays to improve your browsing experience on this site. We use first and third-party cookies and other tracking technologies to obtain website usage data to determine the popularity of this site and online activities. Additionally, we use this information for political and educational management and review, website optimization, and security purposes. For more information on how Microsoft collects and uses your data, please visit the Microsoft Privacy Statement (https://privacy.microsoft.com/privacystatement).'
   },
   bio: 'An open-source project with long history.',
   alternative: ''
@@ -49,30 +43,24 @@ export const zhCN = {
     license: '许可协议',
     version: {
       '0': '本项目',
-      '1': ' 版本由 ',
-      '2': ' 开发。感谢！',
-      stack: '技术栈：',
-      repo: '仓库地址：'
+      '1': '版本由 ',
+      '2': '开发。感谢！'
     },
     thank: {
-      mean: '同时，感谢先前 ',
-      icon: ' 提供的校外服务器，',
-      test: ' 设计的新版（v2.0 - 最新版）图标，以及',
-      end: ' 的疯狂测试。'
+      mean: '同时，感谢先前',
+      icon: '提供的校外服务器，',
+      test: '设计的新版（v2.0 - 最新版）图标，以及',
+      end: '的疯狂测试。'
     },
     v4More: {
       '0': '在 V4 版本发布、投入使用后，',
       '1': '也参与了平台的维护和修缮。'
     }
   },
-  switch: {
-    real: '真名',
-    nick: '昵称'
-  },
   privacy: {
     title: '隐私政策',
     content:
-      '本站与 Microsoft Clarity 和 Microsoft Advertising 合作，通过行为指标、热图和会话重播来捕获您使用本站的方式以及与本站的互动，从而改进本站的浏览体验。我们使用第一和第三方 Cookie 及其他跟踪技术获取网站使用数据，以确定本站的受欢迎程度和在线活动。此外，我们还将此信息用于政教处管理与审查、网站优化、欺诈/安全目的和广告。有关 Microsoft 如何收集和使用您的数据的详细信息，请访问 Microsoft 隐私声明 (https://privacy.microsoft.com/privacystatement)。'
+      '本站与 Microsoft Clarity 和 Microsoft Advertising 合作，通过行为指标、热图和会话重播来捕获您使用本站的方式以及与本站的互动，从而改进本站的浏览体验。我们使用第一和第三方 Cookie 及其他跟踪技术获取网站使用数据，以确定本站的受欢迎程度和在线活动。此外，我们还将此信息用于政教处管理与审查、网站优化和安全目的。有关 Microsoft 如何收集和使用您的数据的详细信息，请访问 Microsoft 隐私声明 (https://privacy.microsoft.com/privacystatement)。'
   },
   bio: '一个历史悠久的开源项目。',
   alternative: '镇海中学义工管理系统'
