a6e7dfabc510991c3c2ac03fd1b12ae0c975d3eb
Zcraft_ <153985444+Zcraft3793@users.noreply.github.com>
2025-03-11 11:52:54 +0800

    file no longer used

--- src/views/notifications/NotificationHome.vue
+++ src/views/notifications/NotificationHome.vue
@@ -1,35 +1,6 @@
 <script lang="ts" setup>
-import ZNotificationList from '@/components/notifications/ZNotificationList.vue'
-import { useRoute, useRouter } from 'vue-router'
-import { ref, watch } from 'vue'
-import { onMounted } from 'vue'
-
-const router = useRouter()
-const route = useRoute()
-
-const mode = ref<'global' | 'personal'>(route.params.type === 'global' ? 'global' : 'personal')
-
-onMounted(() => {
-  if (route.params.type === 'global') mode.value = 'global'
-  else mode.value = 'personal'
-})
-
-watch(
-  () => route.params.type,
-  (newVal) => {
-    if (newVal === 'global') mode.value = 'global'
-    else mode.value = 'personal'
-  },
-  { immediate: true }
-)
-
-const refresh = () => {
-  router.push({ name: 'NotificationHome', params: { mode: mode.value } })
-}
 </script>
 
 <template>
-  <div class="px-8 py-6">
-    <ZNotificationList :refresh="refresh" :mode="mode" />
-  </div>
+  <div />
 </template>
