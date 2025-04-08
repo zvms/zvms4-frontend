1f3c78873a1a6996a426b5a9713ed9e8d906443a
Zcraft_ <153985444+Zcraft3793@users.noreply.github.com>
2025-04-08 11:28:10 +0800

    Update ZActivityDuration.vue

--- src/components/tags/ZActivityDuration.vue
+++ src/components/tags/ZActivityDuration.vue
@@ -15,6 +15,7 @@ const props = defineProps<{
   id?: string
   uid?: string
 }>()
+const emits = defineEmits(['update:duration'])
 
 const { t } = useI18n()
 
@@ -27,6 +28,7 @@ const modification = ref(duration.value)
 async function modify() {
   if (id.value && uid.value && modification > 0 && modification <= 18) {
     await api.activity.duration.modify(uid.value, id.value, modification.value)
+    emits('update:duration', modification.value)
   }
 }
 </script>
