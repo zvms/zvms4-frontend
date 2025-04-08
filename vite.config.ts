e005a7d55cc30ce94d7e12ff43bffbaf532c0934
Zcraft_ <153985444+Zcraft3793@users.noreply.github.com>
2025-04-08 22:33:18 +0800

    try auto update checking (ready to revert if error)

--- vite.config.ts
+++ vite.config.ts
@@ -58,6 +58,29 @@ export default defineConfig({
       workbox: {
         cleanupOutdatedCaches: false,
         sourcemap: true
+      },
+      onRegisteredSW(swUrl, r) {
+        r && setInterval(async () => {
+          if (r.installing || !navigator) {
+            return
+          }
+
+          if ('connection' in navigator && !navigator.onLine) {
+            return
+          }
+          
+          const resp = await fetch(swUrl, {
+            cache: 'no-store',
+            headers: {
+              cache: 'no-store',
+              'cache-control': 'no-cache',
+            },
+          });
+
+          if (resp?.status === 200) {
+            await r.update()
+          }
+        }, 36000000);
       }
     }),
     Icons({}),
