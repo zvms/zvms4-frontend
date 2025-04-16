a869c997db6160ee83c9ab1e09bc20f4288fd4fb
Zcraft_ <153985444+Zcraft3793@users.noreply.github.com>
2025-04-16 22:15:11 +0800

    Update manage.ts

--- src/i18n/locales/manage.ts
+++ src/i18n/locales/manage.ts
@@ -5,8 +5,7 @@ export const enUS = {
       name: 'Name',
       type: 'Type',
       list: 'List',
-      prompt: 'NOTE: Only activities that is the same type and is effective can be merged. You also need to ensure them in a same page,\n' +
-        'otherwise the system will only merge the activities in the same page.',
+      prompt: 'NOTE: Only activities that is the same type and is effective can be merged.',
       overwrite: 'Overwrite',
       overwriteInfo: 'If you enable this option, if there exists duplicated users in activities waiting to be merged, its result time will be the max one in one of existed activities; otherwise it will be the sum.',
       overwritePrompt: {
@@ -116,7 +115,7 @@ export const zhCN = {
       name: '名称',
       type: '类型',
       list: '列表',
-      prompt: '注意：只有类型相同且有效的活动才能被合并。您还需要确保它们在同一页，否则系统只会合并同一页的活动。',
+      prompt: '注意：只有类型相同且有效的义工才能被合并。',
       overwrite: '覆盖时间',
       overwriteInfo: '如果启用此选项，如果待合并的义工中存在重复用户，其结果时间将是现有义工中的最大时间；否则将是总和。',
       overwritePrompt: {
