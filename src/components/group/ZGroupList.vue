9b30c914c95ecd71e773ebc7e4a8a67404ef46bb
Zcraft_ <153985444+Zcraft3793@users.noreply.github.com>
2025-04-24 16:37:34 +0800

    Update ZGroupList.vue

--- src/components/group/ZGroupList.vue
+++ src/components/group/ZGroupList.vue
@@ -21,10 +21,6 @@ const total = ref(0)
 const userStore = useUserStore()
 const { t } = useI18n()
 
-if (!(userStore.position.includes('admin') || userStore.position.includes('department'))) {
-  router.push('/not-found')
-}
-
 const tableHeight = ref(height.value * 0.6)
 
 watch(height, () => {
