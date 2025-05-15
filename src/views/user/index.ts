a80fa7615e91322347f0acad6e5e513708ad7c21
Zcraft_ <153985444+Zcraft3793@users.noreply.github.com>
2025-05-15 17:08:40 +0800

    refactor: use router guard instead

--- src/views/user/index.ts
+++ src/views/user/index.ts
@@ -1,7 +1,6 @@
 import { temporaryToken } from '@/plugins/short-token.ts'
 import { ElMessageBox } from 'element-plus'
 
-export { default as UserActivity } from './UserActivity.vue'
 export { default as UserHome } from './UserHome.vue'
 export { default as UserLogin } from './UserLogin.vue'
 export { default as UserNav } from './UserNav.vue'
